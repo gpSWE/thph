5723040e471e5672ea3cb7164fe9cee891a23206
Lumenkowski <lumenkowski@gmail.com>
2023-04-05 03:24:01 +0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "./main.css"
+
+window.addEventListener( "DOMContentLoaded", () => {
+
+	document.fonts.ready.then( () => {
+
+		document.body.style.opacity = 1
+		
+		document.querySelector( "svg" ).style.opacity = 1
+		document.querySelector( "p" ).style.opacity = 1
+	} )
+} )
