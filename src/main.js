be92aec78892435f39a3cc416f01b3ac6f54c51a
Lumenkowski <lumenkowski@gmail.com>
2023-04-08 11:27:53 +0500

    Custom world

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import "./main.css"
 import * as THREE from "three"
 
 import Stats from "three/addons/libs/stats.module"
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader"
 import { Octree } from "three/addons/math/Octree"
 import { OctreeHelper } from "three/addons/helpers/OctreeHelper"
 import { Capsule } from "three/addons/math/Capsule"
@@ -24,38 +23,13 @@ async function run() {
 	const clock = new THREE.Clock()
 
 	const scene = new THREE.Scene()
-	scene.background = new THREE.Color( 0x88ccee )
-	scene.fog = new THREE.Fog( 0x88ccee, 0, 50 )
 
 	const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 )
 	camera.rotation.order = "YXZ"
 
-	const fillLight1 = new THREE.HemisphereLight( 0x4488bb, 0x002244, 0.5 )
-	fillLight1.position.set( 2, 1, 1 )
-	scene.add( fillLight1 )
-
-	const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 )
-	directionalLight.position.set( - 5, 25, - 1 )
-	directionalLight.castShadow = true
-	directionalLight.shadow.camera.near = 0.01
-	directionalLight.shadow.camera.far = 500
-	directionalLight.shadow.camera.right = 30
-	directionalLight.shadow.camera.left = - 30
-	directionalLight.shadow.camera.top	= 30
-	directionalLight.shadow.camera.bottom = - 30
-	directionalLight.shadow.mapSize.width = 1024
-	directionalLight.shadow.mapSize.height = 1024
-	directionalLight.shadow.radius = 4
-	directionalLight.shadow.bias = - 0.00006
-	scene.add( directionalLight )
-
 	const renderer = new THREE.WebGLRenderer( { antialias: true } )
 	renderer.setPixelRatio( window.devicePixelRatio )
 	renderer.setSize( window.innerWidth, window.innerHeight )
-	renderer.shadowMap.enabled = true
-	renderer.shadowMap.type = THREE.VSMShadowMap
-	renderer.outputEncoding = THREE.sRGBEncoding
-	renderer.toneMapping = THREE.ACESFilmicToneMapping
 
 	document.body.insertBefore( renderer.domElement, document.body.firstElementChild )
 
@@ -356,35 +330,23 @@ async function run() {
 		}
 	}
 
-	const loader = new GLTFLoader().setPath( "/assets/glb/" )
+	// CUSTOM
 
-	loader.load( "collision-world.glb", ( gltf ) => {
+	const world = new THREE.Object3D()
 
-		scene.add( gltf.scene )
+	scene.add( world )
 
-		worldOctree.fromGraphNode( gltf.scene )
+	{
+		const geometry = new THREE.PlaneGeometry( 100, 100, 100, 100 ).rotateX( - Math.PI / 2 )
+		const material = new THREE.MeshBasicMaterial( { color: 0x000000 } )
+		const mesh = new THREE.Mesh( geometry, material )
+		world.add( mesh )
+	}
 
-		gltf.scene.traverse( child => {
+	worldOctree.fromGraphNode( world )
 
-			if ( child.isMesh ) {
-
-				child.castShadow = true
-				child.receiveShadow = true
-
-				if ( child.material.map ) {
-
-					child.material.map.anisotropy = 4
-				}
-			}
-		} )
-
-		const helper = new OctreeHelper( worldOctree )
-		helper.visible = false
-		scene.add( helper )
-
-		const gui = new GUI( { width: 200 } )
-		gui.add( { debug: false }, "debug" ).onChange( () => { } )
-	} )
+	const helper = new OctreeHelper( worldOctree )
+	scene.add( helper )
 
 	function teleportPlayerIfOob() {
 
